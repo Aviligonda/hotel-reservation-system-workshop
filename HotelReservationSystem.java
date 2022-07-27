11c6998e8a131fe778ae908e29038b1a1b37c10d
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-07-27 20:05:35 +0530

    ADDED : Ability to add special rates

--- HotelReservationSystem.java
+++ HotelReservationSystem.java
@@ -4,13 +4,16 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class HotelReservationSystem {
     static List<Hotel> hotelList = new ArrayList<>();
+    static LinkedList<Hotel> specialList=new LinkedList<>();
 
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Reservation System Program");
+        System.out.println("Normal Rates for customers");
         Hotel hotelOne = new Hotel("LakeWood", 110, 90, 3);
         Hotel hotelTwo = new Hotel("BridgeWood", 190, 50, 4);
         Hotel hotelThree = new Hotel("RidgeWood", 220, 150, 5);
@@ -18,8 +21,16 @@ public class HotelReservationSystem {
         hotelList.add(hotelTwo);
         hotelList.add(hotelThree);
         hotelList.forEach(System.out::println);
+        System.out.println("---------------------------------------------------");
+        System.out.println("Special Rates for Reward Customers is :  ");
+        Hotel hotel = new Hotel("LakeWood", 80, 80, 3);
+        Hotel hotel1 = new Hotel("BridgeWood", 110, 50, 4);
+        Hotel hotel2 = new Hotel("RidgeWood", 100, 40, 5);
+        specialList.add(hotel);
+        specialList.add(hotel1);
+        specialList.add(hotel2);
+        specialList.forEach(System.out::println);
         System.out.println("-----------------------------------");
-        cheapestHotel("2022-01-02", "2022-01-03");
     }
 
     public static void cheapestHotel(String startDate, String endDate) {
